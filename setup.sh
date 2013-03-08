ca8a7c783d68f57d2b1395ba6334bf28a0498215
Matt Bridges <mbridges.91@gmail.com>
2013-03-08 09:07:16 -0600

    Add some more to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+
+set -e
+
+gem install bundler
+bundle
+
+sudo mkdir -p /usr/local/
+sudo chown -R `whoami` /usr/local
+
+# Install GCC + Git
+# mkdir -p ~/tmp
+# cd ~/tmp
+# curl https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg > GCC-10.7-v2.pkg
+# sudo installer -pkg GCC-10.7-v2.pkg -target /
+
 bundle exec chef-solo -c config/solo.rb -j config/node.json
