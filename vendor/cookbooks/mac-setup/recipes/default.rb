451b172d077ff767747efe95036e62efe80126d3
Matt Bridges <mbridges.91@gmail.com>
2013-03-08 17:53:31 -0600

    Prefer all .dmg apps definted in node.json

--- vendor/cookbooks/mac-setup/recipes/default.rb
+++ vendor/cookbooks/mac-setup/recipes/default.rb
@@ -17,35 +17,10 @@ Array(node["zip_apps"]).each do |app_name|
   end
 end
 
-dmg_package "Skype" do
-  source "http://www.skype.com/go/getskype-macosx.dmg"
-end
-
-dmg_package "Sublime Text 2" do
-  dmg_name "sublimetext2"
-  source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg"
-end
-
-dmg_package "VLC" do
-  source "http://sourceforge.net/projects/vlc/files/latest/download"
-end
-
-dmg_package "Google Chrome" do
-  dmg_name "googlechrome"
-  source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-end
-
-dmg_package "Google Drive" do
-  dmg_name "googledrive"
-  source "http://dl.google.com/drive/installgoogledrive.dmg"
-end
-
-dmg_package "Jing" do
-  source "http://download.techsmith.com/jing/mac/jing.dmg"
-  accept_eula true
-end
-
-dmg_package "Virtualbox" do
-  source "http://dlc.sun.com.edgesuite.net/virtualbox/4.0.8/VirtualBox-4.0.8-71778-OSX.dmg"
-  type "mpkg"
+Array(node["dmg_apps"]).each do |dmg|
+  dmg_package dmg['app'] do
+    %w[source owner checksum destination type volumes_dir package_id dmg_name dmg_passphrase accept_eula].each do |attribute|
+      send(attribute, dmg[attribute]) if dmg[attribute]
+    end
+  end
 end
