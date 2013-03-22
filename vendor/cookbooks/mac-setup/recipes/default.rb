e155d9a19d7d51f6efb8d754c0583d565ca93e14
Matt Bridges <mbridges.91@gmail.com>
2013-03-22 18:21:40 -0500

    Begin work of installing my desired git repositories from Github

--- vendor/cookbooks/mac-setup/recipes/default.rb
+++ vendor/cookbooks/mac-setup/recipes/default.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 # Installation stuff goes here
 include_recipe "homebrew"
 include_recipe "dmg"
@@ -5,12 +7,18 @@ include_recipe "zip_app"
 
 homebrew_tap "homebrew/dupes"
 
+#######################################################
+# Install Homebrew Packages
+#######################################################
 node["packages"].each do |package_name|
   package package_name do
     provider Chef::Provider::Package::Homebrew
   end
 end
 
+#######################################################
+# Install .zip based App installers
+#######################################################
 Array(node["zip_apps"]).each do |app_name|
   zip_app_package app_name['name'] do
     %w[app source checksum destination zip_file].each do |attribute|
@@ -19,10 +27,30 @@ Array(node["zip_apps"]).each do |app_name|
   end
 end
 
+#######################################################
+# Install .dmg Apps
+#######################################################
 Array(node["dmg_apps"]).each do |dmg|
   dmg_package dmg['app'] do
     %w[source owner checksum destination type volumes_dir package_id dmg_name dmg_passphrase accept_eula].each do |attribute|
       send(attribute, dmg[attribute]) if dmg[attribute]
+    end
+  end
+end
+
+#######################################################
+# Install Some of my usual Git repositories
+#######################################################
+Array(node["repos"]).each do |repositories|
+  data = OpenStruct.new(repositories)
+  path = File.expand_path(data.path)
+  FileUtils.mkdir_p path
+  data.repos.each do |url|
+    directory_name = url.split("/").last
+    git "#{path}/#{directory_name}" do
+      repository "https://github.com/#{url}.git"
+      reference "master"
+      action :sync
     end
   end
 end
