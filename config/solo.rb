cb06e470249b795fa21b25af77d9e0501b964834
Matt Bridges <mbridges.91@gmail.com>
2013-03-22 18:21:54 -0500

    Prefer simple Pathname#+ to string interpolation

--- config/solo.rb
+++ config/solo.rb
@@ -6,9 +6,9 @@ json_attribs    File.expand_path("../node.json", __FILE__)
 
 # Move all the state stuff from /var/chef. I wish there was a single config
 # # variable for this!
-state_root_path = File.expand_path('~/.chef/state')
-file_cache_path  "#{state_root_path}/cache"
-checksum_path    "#{state_root_path}/checksums"
-sandbox_path     "#{state_root_path}/sandbox"
-file_backup_path "#{state_root_path}/backup"
+state_root_path = Pathname.new(File.expand_path('~/.chef/state'))
+file_cache_path  state_root_path + "cache"
+checksum_path    state_root_path + "checksums"
+sandbox_path     state_root_path + "sandbox"
+file_backup_path state_root_path + "backup"
 cache_options[:path] = file_cache_path
