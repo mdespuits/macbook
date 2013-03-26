3b4f2b08ac7cd2a70a19d62cfb2735783c38b9ef
Matt Bridges <mbridges.91@gmail.com>
2013-03-25 22:38:54 -0500

    Move all data into default.rb

--- vendor/cookbooks/mac-setup/recipes/default.rb
+++ vendor/cookbooks/mac-setup/recipes/default.rb
@@ -7,10 +7,53 @@ include_recipe "zip_app"
 
 homebrew_tap "homebrew/dupes"
 
+Packages = ["ack", "apple-gcc42", "cloc", "bash-completion",
+            "git", "hub", "macvim", "memcached", "node", "rbenv",
+            "rbenv-vars", "ruby-build", "redis", "tree",
+            "the_silver_searcher", "tmux", "zsh"]
+
+ZipApps = [
+  OpenStruct.new( name: "Alfred", source: "http://cachefly.alfredapp.com/Alfred_2.0.1_173.zip"),
+  OpenStruct.new( name: "iTerm", source: "http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip"),
+  OpenStruct.new( name: "BetterSnapTool", source: "http://boastr.de/BetterSnapToolTrial.zip"),
+  OpenStruct.new( name: "Postgres", source: "http://postgresapp.com/download"),
+  OpenStruct.new( name: "Github", source: "https://central.github.com/mac/latest"),
+  OpenStruct.new( name: "Induction", source: "http://inductionapp.com/download"),
+  OpenStruct.new( name: "Firefox", source: "https://download.mozilla.org/?product=firefox-19.0.2&os=osx&lang=en-US"),
+  OpenStruct.new( name: "Mou", source: "http://mouapp.com/download/Mou.zip")
+]
+
+DmgApps = [
+  OpenStruct.new( app: "Skype", source: "http://www.skype.com/go/getskype-macosx.dmg"),
+  OpenStruct.new( app: "VLC", source: "http://sourceforge.net/projects/vlc/files/2.0.5/macosx/vlc-2.0.5.dmg/download"),
+  OpenStruct.new( app: "Sublime Text 2", dmg_name: "sublimetext2", source: "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg"),
+  OpenStruct.new( app: "Google Chrome", dmg_name: "googlechrome", source: "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"),
+  OpenStruct.new( app: "Google Drive", dmg_name: "googledrive", source: "http://dl.google.com/drive/installgoogledrive.dmg"),
+  OpenStruct.new( app: "Virtualbox", source: "http://dlc.sun.com.edgesuite.net/virtualbox/4.0.8/VirtualBox-4.0.8-71778-OSX.dmg", type: "mpkg"),
+  OpenStruct.new( app: "Jing", source: "http://download.techsmith.com/jing/mac/jing.dmg", "accept_eula" => true),
+]
+
+Repositories = [
+  OpenStruct.new( path: "~/code/personal/other", url: "mattdbridges/capistrano-recipes" ),
+  OpenStruct.new( path: "~/code/personal/gems", url: "mattdbridges/dotify" ),
+  OpenStruct.new( path: "~/code/personal/gems", url: "mattdbridges/validates_formatting_of" ),
+  OpenStruct.new( path: "~/code/other-projects", directory: "mcheyne", url: "mattdbridges/mcheyne-bible-app" ),
+  OpenStruct.new( path: "~/code/other-projects", url: "gabetax/twitter-bootstrap-kaminari-views" ),
+  OpenStruct.new( path: "~/code/open-source/javascript", url: "twitter/bootstrap" ),
+  OpenStruct.new( path: "~/code/open-source/javascript", url: "simsalabim/sisyphus" ),
+  OpenStruct.new( path: "~/code/open-source/javascript", url: "visionmedia/mocha" ),
+  OpenStruct.new( path: "~/code/open-source/javascript", url: "xing/wysihtml5" ),
+  OpenStruct.new( path: "~/code/open-source/gems", url: "EmmanuelOga/ffaker" ),
+  OpenStruct.new( path: "~/code/open-source/gems", url: "kevinjalbert/git_statistics" ),
+  OpenStruct.new( path: "~/code/open-source/gems", url: "guard/guard-pow" ),
+  OpenStruct.new( path: "~/code/open-source/gems", url: "github/linguist" ),
+]
+
+
 #######################################################
 # Install Homebrew Packages
 #######################################################
-node["packages"].each do |package_name|
+Packages.each do |package_name|
   package package_name do
     provider Chef::Provider::Package::Homebrew
   end
@@ -19,10 +62,10 @@ end
 #######################################################
 # Install .zip based App installers
 #######################################################
-Array(node["zip_apps"]).each do |app_name|
-  zip_app_package app_name['name'] do
+ZipApps.each do |app|
+  zip_app_package app.name do
     %w[app source checksum destination zip_file].each do |attribute|
-      send(attribute, app_name[attribute]) if app_name[attribute]
+      send(attribute, app.send(attribute)) if app.respond_to? attribute
     end
   end
 end
@@ -30,10 +73,10 @@ end
 #######################################################
 # Install .dmg Apps
 #######################################################
-Array(node["dmg_apps"]).each do |dmg|
-  dmg_package dmg['app'] do
+DmgApps.each do |dmg|
+  dmg_package dmg.app do
     %w[source owner checksum destination type volumes_dir package_id dmg_name dmg_passphrase accept_eula].each do |attribute|
-      send(attribute, dmg[attribute]) if dmg[attribute]
+      send(attribute, dmg.send(attribute)) if dmg.respond_to? attribute
     end
   end
 end
@@ -41,16 +84,14 @@ end
 #######################################################
 # Install Some of my usual Git repositories
 #######################################################
-Array(node["repos"]).each do |repositories|
-  data = OpenStruct.new(repositories)
-  path = File.expand_path(data.path)
+
+Repositories.each do |repository|
+  path = Pathname.new(File.expand_path(repository.path))
   FileUtils.mkdir_p path
-  data.repos.each do |url|
-    directory_name = url.split("/").last
-    git "#{path}/#{directory_name}" do
-      repository "https://github.com/#{url}.git"
-      reference "master"
-      action :sync
-    end
+  dir_name = repository.directory || repository.url.split("/").last
+  git "#{path}/#{dir_name}" do
+    repository "https://github.com/#{repository.url}.git"
+    reference "master"
+    action :sync
   end
 end
